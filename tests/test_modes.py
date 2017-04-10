ee9cae23a5f4f4e12fe14c2b17022e58da25704e
Ryan Richardson <ryananthonyrichardson@gmail.com>
2017-04-10 00:30:44 -0700

    Move existing shows and sounds into their respective modes

--- tests/test_modes.py
+++ tests/test_modes.py
@@ -103,57 +103,6 @@ class TestGame(FullMachineTestCase):
         current_score += 100 # more when 'lit'
         self.assertEqual(current_score, self.machine.game.player.score)
 
-        print(current_score)
-
-        #self.assertTextOnTopSlide("BALL 1    FREE PLAY")
-        #self.advance_time_and_run()
-        #self.assertEqual(4 * 75020, self.machine.game.player.score)
-        #self.assertTextOnTopSlide("300,080")
-
-        ## ball drains
-        #self.machine.default_platform.add_ball_to_device(self.machine.ball_devices.trough)
-        ## wait for highscore display
-        #self.advance_time_and_run(10)
-        #self.assertEqual(0, self.machine.playfield.balls)
-        #self.assertEqual(2, self.machine.game.player.ball)
-
-        ## and it should eject a new ball to the pf
-        #self.advance_time_and_run(20)
-        #self.assertEqual(1, self.machine.playfield.balls)
-
-        ## ball drains again
-        #self.machine.default_platform.add_ball_to_device(self.machine.ball_devices.trough)
-        ## wait for highscore display
-        #self.advance_time_and_run(10)
-        #self.assertEqual(0, self.machine.playfield.balls)
-        #self.assertEqual(3, self.machine.game.player.ball)
-
-        ## and it should eject a new ball to the pf
-        #self.advance_time_and_run(20)
-        #self.assertEqual(1, self.machine.playfield.balls)
-
-        ## ball drains again. game should end
-        #self.machine.default_platform.add_ball_to_device(self.machine.ball_devices.trough)
-        #self.advance_time_and_run(10)
-
-        #self.mock_event('text_input_high_score_complete')
-
-        ## enter high score
-        #self.assertSlideOnTop("high_score_enter_initials")
-        #self.hit_and_release_switch("s_flipper_lower_right")
-        #self.hit_and_release_switch("s_flipper_lower_right")
-        #self.hit_and_release_switch("s_start")  # C
-        #self.advance_time_and_run()
-        #self.assertTextOnTopSlide("C")
-        #self.hit_and_release_switch("s_flipper_lower_right")
-        #self.hit_and_release_switch("s_start")  # CD
-        #self.advance_time_and_run()
-        #self.assertTextOnTopSlide("CD")
-        #self.hit_and_release_switch("s_flipper_lower_right")
-        #self.hit_and_release_switch("s_start")  # CDE
-        #self.advance_time_and_run()
-        #self.assertTextOnTopSlide("CDE")
-
     def begin_gameplay(self):
         self.hit_and_release_switch("s_start")
         self.advance_time_and_run(1)
