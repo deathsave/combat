eb02d488ec074223c102c37dd9a60a7885aabc97
Ryan Richardson <ryananthonyrichardson@gmail.com>
2017-05-21 11:43:58 -0700

    Add barebones hole_score mode (switch logic only)

---
+++ modes/hole_score/code/hole_score.py
@@ -0,0 +1,34 @@
+# This is a template file you can use to add Python code to a mode.
+
+# To use it, and it to <your_machine>/modes/<your_mode>/code/<your_mode>.py
+
+# Also, add a blank file called __init__.py to your machine folder root, and
+# to your "modes" folder, and to your mode's "code" folder. (3 total)
+
+# Also add a "code: your_mode.YourMode" (or whatever you call it) to the
+# "mode:" section of your mode's <mode_name>.yaml configuration file.
+
+from mpf.core.mode import Mode as m
+from mpf.core.delays import DelayManager
+
+class Custom(m):
+    def mode_start(self, **kwargs):
+        print('Collecting hole score bonus')
+        player = self.machine.game.player
+        awarded_score = 0
+        if player.hole_score_count == 10:
+            awarded_score += 50000
+        elif player.hole_score_count > 6:
+            awarded_score += 5000
+        elif player.hole_score_count > 3:
+            awarded_score += 500
+        else:
+            awarded_score += 50
+        player.score += awarded_score
+        # FUTURE? - award fragment of total for each count
+        # for a "bonus collection" experience
+        #awarded_part = awarded_score / player.hole_score_count
+
+        #for i in range(player.hole_score_count):
+        #    player.score += int(awarded_part) # rounding errors...
+        del kwargs
---
+++ tests/test_hole_score.py
@@ -0,0 +1,68 @@
+import os
+from mpfmc.tests.FullMpfMachineTestCase import FullMachineTestCase
+
+class TestHoleScoreMode(FullMachineTestCase):
+
+    def getMachinePath(self):
+        return os.path.abspath(os.path.join(os.path.realpath(__file__), os.pardir, os.pardir))
+
+    def test_default(self):
+        current_score = self._init_game()
+        # wah, wah... only 50 points collected if you don't advance the hole
+        self.hit_and_release_switch("s_kicker_saucer")
+        current_score += 50
+        self.assertEqual(current_score, self.machine.game.player.score)
+
+    def test_low_block(self):
+        current_score = self._init_game()
+        # begin at 1, in low throughin first block for up to two advancements
+        for i in range(2):
+            self.hit_and_release_switch("s_stationary_advance_hole_score")
+        self.hit_and_release_switch("s_kicker_saucer")
+        self.assertModeRunning('hole_score')
+        current_score += 50
+        self.assertEqual(current_score, self.machine.game.player.score)
+        # 550 != 1000
+
+    def test_mid_block(self):
+        current_score = self._init_game()
+        # begin at 1, in mid through 6
+        for i in range(5):
+            self.hit_and_release_switch("s_stationary_advance_hole_score")
+        self.hit_and_release_switch("s_kicker_saucer")
+        self.assertModeRunning('hole_score')
+        current_score += 500
+        self.assertEqual(current_score, self.machine.game.player.score)
+        # 1000 != 5500
+
+    def test_high_block(self):
+        current_score = self._init_game()
+        # begin at 1, in high through 9
+        for i in range(8):
+            self.hit_and_release_switch("s_stationary_advance_hole_score")
+        self.hit_and_release_switch("s_kicker_saucer")
+        self.assertModeRunning('hole_score')
+        current_score += 5000
+        self.assertEqual(current_score, self.machine.game.player.score)
+
+    def test_max_value(self):
+        current_score = self._init_game()
+        # 10 advancements for max value...
+        for i in range(1,10):
+            self.hit_and_release_switch("s_stationary_advance_hole_score")
+        self.hit_and_release_switch("s_kicker_saucer")
+        self.assertModeRunning('hole_score')
+        current_score += 50000
+        self.assertEqual(current_score, self.machine.game.player.score)
+
+    def _init_game(self):
+        current_score = 0
+        self.hit_and_release_switch("s_start")
+        self.advance_time_and_run(1)
+        # activate base mode
+        self.hit_and_release_switch("s_rollover_top_1")
+        self.advance_time_and_run(4) # 3+1
+        current_score += 500
+        self.assertEqual(1, self.machine.playfield.balls)
+        self.assertEqual(current_score, self.machine.game.player.score)
+        return current_score
