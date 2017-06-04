aa281e77b0a0cea07dcd8a0e17242989aea04ab3
Ryan Richardson <ryananthonyrichardson@gmail.com>
2017-06-03 22:15:35 -0700

    Add explosion bonus multiplier

--- modes/explosion_multiplier/code/explosion_multiplier.py
+++ modes/explosion_multiplier/code/explosion_multiplier.py
@@ -11,14 +11,14 @@
 from mpf.core.mode import Mode as m
 
 class Custom(m):
+    def mode_init(self):
+        """This code that will run once mode when MPF boots."""
+        pass
+
     def mode_start(self, **kwargs):
-        #   NICE TO HAVE - unlight each led and fire a sound in sequence
-        player = self.machine.game.player
-        base_bonus_score = ((player.explosion_bonus_count - 1) * 1000)
-        # TODO x10 if gun was ever hit (x10 led lit)
-        # TODO base_bonus_score = 0 (if TILTED)
-        if self.machine.modes.explosion_multiplier.active:
-            player.score += (base_bonus_score * 10)
-        else:
-            player.score += base_bonus_score
+        """This code will run every time this mode starts."""
         del kwargs
+
+    def mode_stop(self, **kwargs):
+        """This code will run every time this mode stops."""
+        pass
--- tests/test_explosion_bonus.py
+++ tests/test_explosion_bonus.py
@@ -6,7 +6,7 @@ class TestExplosionBonusMode(FullMachineTestCase):
     def getMachinePath(self):
         return os.path.abspath(os.path.join(os.path.realpath(__file__), os.pardir, os.pardir))
 
-    def test_scoring(self):
+    def test_normal_scoring(self):
         current_score = 0
         self.hit_and_release_switch("s_start")
         self.advance_time_and_run(1)
@@ -37,4 +37,44 @@ class TestExplosionBonusMode(FullMachineTestCase):
         self.assertEqual(2, self.machine.game.player.ball)
         # at the end of the ball, the player collects the bonus
         current_score += 5000
+        self.assertEqual(current_score, self.machine.game.player.score)
+
+    def test_multiplied_scoring(self):
+        current_score = 0
+        self.hit_and_release_switch("s_start")
+        self.advance_time_and_run(1)
+
+        # activate base mode
+        self.hit_and_release_switch("s_rollover_top_1")
+        self.advance_time_and_run(4) # 3+1
+        current_score += 500
+        self.assertEqual(1, self.machine.playfield.balls)
+        self.assertEqual(current_score, self.machine.game.player.score)
+
+        # advance the bonus with target 5 times
+        for x in range(0, 5):
+            self.hit_and_release_switch("s_stationary_advance_bonus")
+            # bonus points not added now...
+            # just the base 100 points for each
+            current_score += 100
+        self.assertEqual(current_score, self.machine.game.player.score)
+
+        # TODO: advance the bonus with LIT rollover
+        # self.hit_and_release_switch("s_rollover_top_1")
+
+        # wait for "shoot again" to expire
+        self.advance_time_and_run(15)
+
+        # trigger the Gun to activate 10x
+        self.hit_and_release_switch("s_kicker_gun")
+        self.advance_time_and_run(1) # wait to get into the mode
+        self.hit_and_release_switch("s_kicker_gun")
+        current_score += 5000 # score for hitting gun
+        self.assertModeRunning('explosion_multiplier')
+
+        # drain and collect bonus
+        self.hit_switch_and_run('s_trough_1', 2)
+        self.assertEqual(2, self.machine.game.player.ball)
+        # at the end of the ball, the player collects the bonus
+        current_score += 50000
         self.assertEqual(current_score, self.machine.game.player.score)
