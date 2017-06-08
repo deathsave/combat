073f96c7e12a166d6df013b7f6c229079066d0f8
Ryan Richardson <ryananthonyrichardson@gmail.com>
2017-06-08 15:48:29 -0700

    Reincorporate gameover since NWPAS 17

--- modes/high_score/code/high_score.py
+++ modes/high_score/code/high_score.py
@@ -4,7 +4,14 @@ class Custom(m):
     def mode_init(self):
         pass
 
+    def set_high_score_indication(self, **kwargs):
+        self.machine.is_high_score_set = True
+        pass
+
     def mode_start(self, **kwargs):
+        self.machine.events.add_handler(
+            'high_score_enter_initials', self.set_high_score_indication)
+
         del kwargs
 
     def mode_stop(self, **kwargs):
--- modes/initialize/code/initialize.py
+++ modes/initialize/code/initialize.py
@@ -11,28 +11,11 @@
 from mpf.core.mode import Mode
 
 class Custom(Mode):
-    # MPF persists player's scores so they can display in the attact
-    # mode, but then they akwardly display immediately in a new game
-    # until scoring happens. this clears them
-    def initialize_player_scores(self, **kwargs):
-        # TODO: this is not working. cant seem to clear the persisted
-        # values for scores between games... (issue #939 in MPF core)
-        for player in self.machine.game.player_list:
-            self.machine.remove_machine_var(
-                name='player{}_score'.format(player.number))
-            self.machine.create_machine_var(
-                'player{}_score'.format(player.number),
-                value=0,
-                persist=False,
-                expire_secs=None,
-                silent=False)
-        del kwargs
-
     def track_score_levels(self, **kwargs):
         # Tracks the player's score to see if they are entitled
         # to an extra ball at various levels (currently only 1 level)
         player = self.machine.game.player
-        if player.score_level > 0:
+        if player and player.score_level > 0:
             return
         elif player.score_level == 0 and player.score >= 250000:
             self.machine.extra_balls.score_level_1.award()
@@ -41,8 +24,6 @@ class Custom(Mode):
         del kwargs
 
     def mode_init(self):
-        self.machine.events.add_handler(
-            'multiplayer_game', self.initialize_player_scores)
         self.machine.events.add_handler(
             'player_score', self.track_score_levels)
 
