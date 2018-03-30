30c0074e4efc0193a93c3f85f7bff9af17d6746c
Ryan Richardson <ryananthonyrichardson@gmail.com>
2018-03-29 22:19:55 -0700

    Fix 'existing' DMD test (which doesn't prove much)

--- tests/test_attract.py
+++ tests/test_attract.py
@@ -7,15 +7,17 @@ class TestAttractMode(FullMachineTestCase):
         return os.path.abspath(os.path.join(os.path.realpath(__file__), os.pardir, os.pardir))
 
     def test_dmd_display(self):
-        self.advance_time_and_run()
+        self.advance_time_and_run(2)
         # verify widget is on the DMD
         current_widgets = self.get_dmd_text_widgets()
+        self.assertIn('Zaccaria “Combat”', [x.text for x in current_widgets])
+        self.assertIn('Reimagined by:', [x.text for x in current_widgets])
         self.assertIn('Pinball Plaid', [x.text for x in current_widgets])
         # stop the mode and verify the widget has been removed
         self.hit_and_release_switch("s_start")
         self.advance_time_and_run()
         current_widgets = self.get_dmd_text_widgets()
-        self.assertNotIn('Pinball Plaid', [x.text for x in current_widgets])
+        self.assertNotIn('Zaccaria “Combat”', [x.text for x in current_widgets])
 
     #############
     ## Helpers ##
@@ -23,12 +25,7 @@ class TestAttractMode(FullMachineTestCase):
 
     def get_dmd_text_widgets(self):
         current_widgets = []
-        virtual_dmd = self.mc.displays['dmd'].\
-            children[0].\
-            children[0].\
-            children[0].\
-            children[0].\
-            children[0]
-        for widget in virtual_dmd.children:
-            if hasattr(widget, 'text'): current_widgets.append(widget)
+        for widget_container in self.mc.displays['dmd'].current_slide.widgets:
+            if hasattr(widget_container.widget, 'text'): current_widgets.\
+                append(widget_container.widget)
         return current_widgets
