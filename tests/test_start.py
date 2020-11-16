0840b6d5a7a50be126a3c2cfcca8d5cd6c40ceb4
Ryan Richardson <ryananthonyrichardson@gmail.com>
2020-11-15 19:15:23 -0800

    WIP, per 0.54.1 playfield_active change

--- tests/test_start.py
+++ tests/test_start.py
@@ -4,7 +4,9 @@ from mpf.tests.MpfMachineTestCase import MpfMachineTestCase
 class TestGameStart(MpfMachineTestCase):
 
     def getMachinePath(self):
-        return os.path.abspath(os.path.join(os.path.realpath(__file__), os.pardir, os.pardir))
+        return os.path.abspath(os.path.join(
+            os.path.realpath(__file__), os.pardir, os.pardir)
+        )
 
     def test_game_start(self):
         self.hit_and_release_switch("s_start")
@@ -15,13 +17,19 @@ class TestGameStart(MpfMachineTestCase):
         ## playfield expects a ball
         self.assertEqual(1, self.machine.playfield.available_balls)
         ## ball is ejected to plunger
+        self.assertEqual(
+            1, self.machine.ball_devices["bd_shooter_lane"].balls
+        )
         self.assertEqual(0, self.machine.playfield.balls)
         ## player launches a ball
+        self.hit_and_release_switch('s_shooter_lane')
+        self.advance_time_and_run()
         ## and it hits a playfield switch
         self.hit_and_release_switch("s_rollover_top_2")
         ## not immediately "on the playfield", though
-        self.assertEqual(0, self.machine.playfield.balls)
+        self.assertBallsOnPlayfield(0)
         ## after a brief delay, the ball is now on the playfield...
         self.advance_time_and_run()
-        self.assertEqual(1, self.machine.playfield.balls)
+        self.assertEventCalled("playfield_active")
+        self.assertBallsOnPlayfield(1)
 
