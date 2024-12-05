a87bee51745f5384741273ff26d27690a698f8b7
Ryan <ryananthonyrichardson@gmail.com>
2024-12-04 18:28:11 -0800

    test: fix ball saves test

--- tests/test_ball_saves.py
+++ tests/test_ball_saves.py
@@ -9,25 +9,46 @@ class TestBallSaves(MpfMachineTestCase):
     def test_timer(self):
         self.hit_and_release_switch("s_start")
         self.advance_time_and_run(1)
+        self.hit_and_release_switch("s_shooter_lane")
 
         # timer begins on first switch hit
-        self.hit_and_release_switch("s_slingshot_1")
+        self.hit_and_release_switch("s_spinner")
+        self.assertTrue(self.machine.
+            ball_saves["default"].enabled)
         self.advance_time_and_run(11) # within 2s grace period
 
         # draining before timer expires will save the ball
         self.hit_switch_and_run('s_trough_1', 4)
         self.assertEqual(1, self.machine.game.player.ball)
+        self.assertEqual(1,
+            self.machine.ball_devices.bd_shooter_lane.balls)
+        self.assertFalse(self.machine.
+            ball_saves["default"].enabled)
+        self.hit_and_release_switch("s_shooter_lane")
+        self.hit_and_release_switch("s_spinner")
 
         # playing/draining again ends the ball
-        self.hit_and_release_switch("s_slingshot_1")
         self.hit_switch_and_run('s_trough_1', 4)
         self.assertEqual(2, self.machine.game.player.ball)
+        self.assertEqual(1,
+            self.machine.ball_devices.bd_shooter_lane.balls)
+        self.assertTrue(self.machine.
+            ball_saves["default"].enabled)
+        self.hit_and_release_switch("s_shooter_lane")
+        self.hit_and_release_switch("s_spinner")
 
         # wating to drain after timer expiry
         # will NOT save the ball
-        self.hit_and_release_switch("s_slingshot_1")
-        self.advance_time_and_run(12) # 10s+2s grace
+        self.advance_time_and_run(10) # 10s+2s grace
+        self.assertTrue(self.machine.
+            ball_saves["default"].enabled)
+
+        # after 2s grace, we lose the ball
+        self.advance_time_and_run(2)
+        self.assertFalse(self.machine.
+            ball_saves["default"].enabled)
         self.hit_switch_and_run('s_trough_1', 4)
+        self.advance_time_and_run(4)
         self.assertEqual(3, self.machine.game.player.ball)
 
     def test_laser_bombs_outlane(self):
