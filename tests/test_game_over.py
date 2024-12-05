df56a285a100e5b99e822b095f49a61afe012170
Ryan <ryananthonyrichardson@gmail.com>
2024-12-04 17:58:35 -0800

    test: fix game over / high score tests

--- tests/test_game_over.py
+++ tests/test_game_over.py
@@ -5,38 +5,43 @@ class TestGameOverMode(MpfMachineTestCase):
 
     def test_game_over(self):
         self.hit_and_release_switch("s_start")
-        self.advance_time_and_run(2)
+        self.hit_and_release_switch("s_shooter_lane")
+        self.hit_switch_and_run('s_rollover_top_2', 4)
 
         # drain all 3 balls
         for ball in range(1, 4):
             self.assertEqual(ball, self.machine.game.player.ball)
             # ball drains twice (ball save)
             for i in range(0, 2):
-                self.hit_switch_and_run('s_trough_1', 2)
+                self.hit_and_release_switch("s_shooter_lane")
+                self.hit_switch_and_run('s_trough_1', 4)
 
         self.assertModeRunning('game_over')
 
     def test_game_over_with_high_score_set(self):
+        current_score = 0
         self.hit_and_release_switch("s_start")
-        self.advance_time_and_run(2)
+        self.hit_and_release_switch("s_shooter_lane")
+        self.hit_switch_and_run('s_rollover_top_2', 4)
 
-        # hit a ton of drop targets...
-        for x in range(0, 20):
-            self.hit_and_release_switch("s_kicker_gun")
+        # exceeds 50k points to set low end of board
+        for x in range(0, 51):
+            current_score += 1000
+            self.hit_and_release_switch("s_stationary_thousand")
 
-        # drain all 3 balls (plus bonus ball for hitting score level 1
-        for ball in range(1, 5):
+        # drain all 3 balls
+        for ball in range(1, 4):
             # ball drains twice (ball save)
             for i in range(0, 2):
-                self.hit_switch_and_run('s_trough_1', 2)
+                self.hit_and_release_switch("s_shooter_lane")
+                self.hit_switch_and_run('s_trough_1', 4)
 
-        self.advance_time_and_run(2)
         self.assertModeRunning('high_score')
 
         # enter initials as "AAA"
         for x in range(0, 3):
-            self.advance_time_and_run(2)
             self.hit_and_release_switch("s_start")
+            self.advance_time_and_run(4)
 
-        self.advance_time_and_run(9)
+        self.advance_time_and_run(5)
         self.assertModeRunning('game_over')
