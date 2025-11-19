8c67e22dd268736d94e0fcefa6fa9046c6f20d53
Ryan <ryananthonyrichardson@gmail.com>
2025-11-18 18:12:26 -0800

    feat: add hole score redemption call outs

--- modes/hole_score/code/hole_score.py
+++ modes/hole_score/code/hole_score.py
@@ -12,15 +12,19 @@ class Custom(Mode):
             if player.hole_score_count >= 10:
                 print("Awarding 50000 for hole score")
                 awarded_score += 50000
+                self.machine.events.post("hole_score_redeem_max")
             elif player.hole_score_count > 6:
                 print("Awarding 5000 for hole score")
                 awarded_score += 5000
+                self.machine.events.post("hole_score_redeem")
             elif player.hole_score_count > 3:
                 print("Awarding 500 for hole score")
                 awarded_score += 500
+                self.machine.events.post("hole_score_redeem")
             else:
                 print("Hole score count less than 4, awarding 100 per hole")
                 awarded_score += 50
+                self.machine.events.post("hole_score_redeem")
             if self.machine.modes.multiball.active:
                 print("Awarding double hole score")
                 awarded_score *= 2
